c83c54df6c2ac999c47333e150dc0d3798bed65f
Reanal Em <reem@paypal.com>
2020-01-31 11:15:58 -0800

    Second commit of file2.java

--- file2.java
+++ file2.java
@@ -1,2 +1,8 @@
 This is the second code in the project
-
+This is the second code in the project
+This is the second code in the project
+This is the second code in the project
+This is the second code in the project
+This is the second code in the project
+This is the second code in the project
+This is the second code in the project
